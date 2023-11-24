5265365476e31ce5823199ec4848e50c4820c751
Alpha-Coder02 <103884994+kashiraza01@users.noreply.github.com>
2023-11-24 18:35:59 +0500

    Q6 made for input and displaying of file data
    You can display data of file in this from Q6.txt

---
+++ Q6.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Q6 {
+    public static void main(String[] args) {
+        String filePath = "Q6.txt"; // Replace with the actual file path
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+}
